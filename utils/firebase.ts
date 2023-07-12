bffdc80debbd692ce34111e3578af765c4d80f88
franklinnnn <franklin.assa@gmail.com>
2023-07-11 17:30:51 -0700

    update

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,14 +1,14 @@
-import { getFirestore } from "firebase-admin/firestore";
-import { getApp, getApps, initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase-admin/firestore";
+// import { getApp, getApps, initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "carzo-1e146.firebaseapp.com",
-  projectId: "carzo-1e146",
-  storageBucket: "carzo-1e146.appspot.com",
-  messagingSenderId: "713122479069",
-  appId: "1:713122479069:web:fd228325086538c0fc9434",
-};
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: "carzo-1e146.firebaseapp.com",
+//   projectId: "carzo-1e146",
+//   storageBucket: "carzo-1e146.appspot.com",
+//   messagingSenderId: "713122479069",
+//   appId: "1:713122479069:web:fd228325086538c0fc9434",
+// };
 
-export const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-export const db = getFirestore();
+// export const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+// export const db = getFirestore();
